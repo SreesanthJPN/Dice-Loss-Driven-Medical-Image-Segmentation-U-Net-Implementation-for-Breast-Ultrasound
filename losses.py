9d5d238889b4eac013c9b40a9165b1eec25c7a28
SreesanthJPN <157267306+SreesanthJPN@users.noreply.github.com>
2024-09-19 12:00:48 +0530

    Update losses.py

--- losses.py
+++ losses.py
@@ -2,21 +2,10 @@ import torch
 import torch.nn.functional as F
 
 def dice_loss(y_pred, y_test):
-    """Dice Loss function for binary classification.
-    
-    pred: tensor with first dimension as batch (logits before sigmoid)
-    target: tensor with first dimension as batch (binary ground truth)
-    
-    Returns the Dice Loss, which is differentiable.
-    """
-    
     smooth = 1.0
-    
     pred = torch.sigmoid(y_pred)
-    
     if pred.size() != y_test.size():
         pred = F.interpolate(pred, size=y_test.size()[2:], mode='bilinear', align_corners=False)
-    
     iflat = pred.contiguous().view(-1)
     tflat = y_test.contiguous().view(-1)
     intersection = (iflat * tflat).sum()
